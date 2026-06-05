d80c10791583b4c88b2de1bbc57d6c3ec81dd8bc
GeroA6 <aguerogeronimo954@gmail.com>
2026-06-05 18:39:55 -0300

    Primer Commit
    Es la base del manos a la obra I. Solo html y css

--- mi-app-react/src/App.tsx
+++ mi-app-react/src/App.tsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   return (
-    <main>
-      <h1>Mi primera app React</h1>
-      <p>App creada con Vite y TypeScript.</p>
+    <main className="app-container">
+      <h1>Lista de Tareas</h1>
+      
+      <section className="input-container">
+        <input className="text-input" placeholder="Escribe una tarea" />
+        <button className="submit-button">ADD</button>
+      </section>
+
+      <ul className="list-container">
+        <li className="list-item">
+          <input type="checkbox" />
+          <p>Tarea 1</p>
+          <button className="delete-button">X</button>
+        </li>
+        <li className="list-item">
+          <input type="checkbox" />
+          <p>Tarea 2</p>
+          <button className="delete-button">X</button>
+        </li>
+        <li className="list-item">
+          <input type="checkbox" />
+          <p>Tarea 3</p>
+          <button className="delete-button">X</button>
+        </li>
+      </ul>
     </main>
   )
 }
